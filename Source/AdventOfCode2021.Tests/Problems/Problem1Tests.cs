83bb12684c6042cf11c2cab5ae6b45dabcde1382
Joel Ahlgren <joel@ahlgren.io>
2021-12-01 08:49:00 +0100

    Solved day 1.

--- Source/AdventOfCode2021.Tests/Problems/Problem1Tests.cs
+++ Source/AdventOfCode2021.Tests/Problems/Problem1Tests.cs
@@ -6,5 +6,29 @@ using NUnit.Framework;
 [TestFixture]
 public class Problem1Tests
 {
+    private readonly string[] _testInput = new[] 
+    {   
+        "199",
+        "200",
+        "208",
+        "210",
+        "200",
+        "207",
+        "240",
+        "269",
+        "260",
+        "263"
+    };
 
+    [Test]
+    public void TestPart1()
+    {
+        Assert.AreEqual(7, Problem1.PartOne(_testInput));
+    }
+
+    [Test]
+    public void TestPart2()
+    {
+        Assert.AreEqual(5, Problem1.PartTwo(_testInput));
+    }
 }
--- Source/AdventOfCode2021/Problems/Problem1.cs
+++ Source/AdventOfCode2021/Problems/Problem1.cs
@@ -1,5 +1,9 @@
 namespace AdventOfCode2021.Problems;
 
+using System.Collections.Generic;
+
+using Utils.Extensions;
+
 /// <summary>
 /// Solution for <a href="https://adventofcode.com/2021/day/1">Day 1</a>.
 /// </summary>
@@ -10,12 +14,49 @@ public class Problem1 : ProblemBase
     /// <inheritdoc />
     protected override object SolvePartOne()
     {
-        return "Unsolved";
+        return PartOne(Input);
     }
 
     /// <inheritdoc />
     protected override object SolvePartTwo()
     {
-        return "Unsolved";
+        return PartTwo(Input);
+    }
+
+    internal static int PartOne(ICollection<string> input)
+    {
+        var measurements = input.ConvertToInt();
+
+        var count = 0;
+
+        for (var i = 0; i < measurements.Count - 1; i++)
+        {
+            if (measurements[i] < measurements[i + 1])
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    internal static int PartTwo(ICollection<string> input)
+    {
+        var measurements = input.ConvertToInt();
+
+        var count = 0;
+
+        for (var i = 0; i < measurements.Count - 3; i++)
+        {
+            var groupOne = measurements[i] + measurements[i + 1] + measurements[i + 2];
+            var groupTwo = measurements[i+1] + measurements[i + 2] + measurements[i + 3];
+
+            if (groupTwo > groupOne)
+            {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
