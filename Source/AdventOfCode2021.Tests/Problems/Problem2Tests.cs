df4f47e8335d3c1c033b2effa3c9972404b349c0
Joel Ahlgren <joel@ahlgren.io>
2021-12-02 09:25:21 +0100

    Solved day 2.

--- Source/AdventOfCode2021.Tests/Problems/Problem2Tests.cs
+++ Source/AdventOfCode2021.Tests/Problems/Problem2Tests.cs
@@ -7,5 +7,25 @@ using NUnit.Framework;
 [TestFixture]
 public class Problem2Tests
 {
+    private string[] _testInput = new string[]
+    {
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2"
+    };
 
+    [Test]
+    public void TestPartOne()
+    {
+        Assert.AreEqual(150, Problem2.PartOne(_testInput));
+    }
+
+    [Test]
+    public void TestPartTwo()
+    {
+        Assert.AreEqual(900, Problem2.PartTwo(_testInput));
+    }
 }
--- Source/AdventOfCode2021/Problems/Problem2.cs
+++ Source/AdventOfCode2021/Problems/Problem2.cs
@@ -1,5 +1,10 @@
 namespace AdventOfCode2021.Problems;
 
+using System;
+using System.Collections.Generic;
+
+using AdventOfCode2021.Utils.Extensions;
+
 /// <summary>
 /// Solution for <a href="https://adventofcode.com/2021/day/2">Day 2</a>.
 /// </summary>
@@ -10,12 +15,64 @@ public class Problem2 : ProblemBase
     /// <inheritdoc />
     protected override object SolvePartOne()
     {
-        return "Unsolved";
+        return PartOne(Input);
     }
 
     /// <inheritdoc />
     protected override object SolvePartTwo()
     {
-        return "Unsolved";
+        return PartTwo(Input);
+    }
+
+    internal static int PartOne(ICollection<string> input)
+    {
+        var parsedInput = new Dictionary<string, int>();
+
+        foreach (var line in input)
+        {
+            var tmp = line.Split(" ");
+
+            if (!parsedInput.ContainsKey(tmp[0]))
+            {
+                parsedInput.Add(tmp[0], 0);
+            }
+
+            parsedInput[tmp[0]] += tmp[1].ToInt();
+        }
+
+        var horizontal = parsedInput["forward"];
+        var vertical = parsedInput["down"] - parsedInput["up"];
+
+        return horizontal * vertical;
+    }
+
+    internal static int PartTwo(ICollection<string> input)
+    {
+        var aim = 0;
+        var horizontal = 0;
+        var depth = 0;
+
+        foreach (var line in input)
+        {
+            var tmp = line.Split(" ");
+
+            switch (tmp[0])
+            {
+                case "forward":
+                    horizontal += tmp[1].ToInt();
+                    depth += aim * tmp[1].ToInt();
+                    break;
+                case "up":
+                    aim -= tmp[1].ToInt();
+                    break;
+                case "down":
+                    aim += tmp[1].ToInt();
+                    break;
+                default:
+                    throw new Exception("Oh no");
+            }
+        }
+
+        return horizontal * depth;
     }
 }
