7d8df5ccdeb5bae2dc61ea2676194f4f60b77cc8
Joel Ahlgren <joel@ahlgren.io>
2021-12-06 09:19:06 +0100

    Day 6, part 1 solved with naive approach, part 2 delayed.

--- Source/AdventOfCode2021.Tests/Problems/Problem6Tests.cs
+++ Source/AdventOfCode2021.Tests/Problems/Problem6Tests.cs
@@ -7,5 +7,21 @@ using NUnit.Framework;
 [TestFixture]
 public class Problem6Tests
 {
+    private readonly string[] _testInput = new[]
+    {
+        "3,4,3,1,2"
+    };
 
+    [Test]
+    public void TestPartOne()
+    {
+        Assert.AreEqual(26, Problem6.PartOne(_testInput, 18));
+        Assert.AreEqual(5934, Problem6.PartOne(_testInput, 80));
+    }
+
+    [Test]
+    public void TestPartTwo()
+    {
+        Assert.AreEqual(26984457539, Problem6.PartOne(_testInput, 256));
+    }
 }
--- Source/AdventOfCode2021/Problems/Problem6.cs
+++ Source/AdventOfCode2021/Problems/Problem6.cs
@@ -1,5 +1,10 @@
 namespace AdventOfCode2021.Problems;
 
+using System.Collections.Generic;
+using System.Linq;
+
+using AdventOfCode2021.Utils.Extensions;
+
 /// <summary>
 /// Solution for <a href="https://adventofcode.com/2021/day/6">Day 6</a>.
 /// </summary>
@@ -10,12 +15,39 @@ public class Problem6 : ProblemBase
     /// <inheritdoc />
     protected override object SolvePartOne()
     {
-        return "Unsolved";
+        return PartOne(Input, 80);
     }
 
     /// <inheritdoc />
     protected override object SolvePartTwo()
     {
-        return "Unsolved";
+        return PartOne(Input, 256);
+    }
+
+    internal static long PartOne(ICollection<string> input, int days)
+    {
+        var fishPond = input.First().Split(',').ConvertToInt();
+
+        for (var day = 0; day < days; day++)
+        {
+            for (var i = 0; i < fishPond.Count; i++)
+            {
+                if (fishPond[i] == 0)
+                {
+                    fishPond.Add(9);
+                }
+                if (fishPond[i] == -1)
+                {
+                    fishPond[i] = 6;
+                }
+            }
+
+            for (var i = 0; i < fishPond.Count; i++)
+            {
+                fishPond[i]--;
+            }
+        }
+
+        return fishPond.Count;
     }
 }
