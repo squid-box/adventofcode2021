97c691855214c0fe85e515ca24233d1dd6e9d082
Joel Ahlgren <joel.ahlgren@tobiidynavox.com>
2021-12-07 20:36:28 +0100

    Solved day 7.

--- Source/AdventOfCode2021.Tests/Problems/Problem7Tests.cs
+++ Source/AdventOfCode2021.Tests/Problems/Problem7Tests.cs
@@ -7,5 +7,20 @@ using NUnit.Framework;
 [TestFixture]
 public class Problem7Tests
 {
+    private readonly string[] _testInput = new string[]
+    {
+        "16,1,2,0,4,2,7,1,2,14"
+    };
 
+    [Test]
+    public void TestPartOne()
+    {
+        Assert.AreEqual(37, Problem7.FindLowestFuelConsumption(_testInput, 1));
+    }
+
+    [Test]
+    public void TestPartTwo()
+    {
+        Assert.AreEqual(168, Problem7.FindLowestFuelConsumption(_testInput, 2));
+    }
 }
--- Source/AdventOfCode2021/Problems/Problem7.cs
+++ Source/AdventOfCode2021/Problems/Problem7.cs
@@ -1,5 +1,11 @@
 namespace AdventOfCode2021.Problems;
 
+using AdventOfCode2021.Utils.Extensions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 /// <summary>
 /// Solution for <a href="https://adventofcode.com/2021/day/7">Day 7</a>.
 /// </summary>
@@ -10,12 +16,50 @@ public class Problem7 : ProblemBase
     /// <inheritdoc />
     protected override object SolvePartOne()
     {
-        return "Unsolved";
+        return FindLowestFuelConsumption(Input, 1);
     }
 
     /// <inheritdoc />
     protected override object SolvePartTwo()
     {
-        return "Unsolved";
+        return FindLowestFuelConsumption(Input, 2);
+    }
+
+    internal static long FindLowestFuelConsumption(ICollection<string> input, int part)
+    {
+        var crabs = input.First().Split(',').ConvertToInt();
+
+        // Fuel cost: (position, cost)
+        var fuelCostSummary = new List<(int, long)>();
+
+        // Test all positions and evaluate fuel cost.
+        for (var pos = crabs.Min(); pos <= crabs.Max(); pos++)
+        {
+            var cost = 0L;
+
+            foreach (var crab in crabs)
+            {
+                if (crab == pos)
+                {
+                    continue;
+                }
+
+                if (part == 1)
+                {
+                    cost += Math.Abs(crab - pos);
+                }
+                else
+                {
+                    for (var i = 0; i < Math.Abs(crab - pos); i++)
+                    {
+                        cost += 1 + i;
+                    }
+                }                
+            }
+
+            fuelCostSummary.Add((pos, cost));
+        }
+
+        return fuelCostSummary.Min(x => x.Item2);
     }
 }
